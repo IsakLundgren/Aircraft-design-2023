ddfe4bd38676224db1117299655e0541aba6990a
IsakLundgren <isak.leopold@gmail.com>
2023-08-29 16:58:49 +0200

    Added all blocks needed

--- Main.py
+++ Main.py
@@ -1,6 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Important notes:
+# Camel case for all parameters, underscore means fraction
+# No capital for next word if prev word is 1 letter long
+
 ### Predefined parameters
 
 passengerWeight = 105 # kg
@@ -11,17 +15,78 @@ passengerCount = 90
 ## Crew and payload weight calculation
 
 # In
+crewCount = 5
 
 # Out
-W_crew = 1
-W_payload = 1
+WCrew = 1
+WPayload = 1
+
+## Lift to drag ratio
+
+# In
+Swet_Sref = 1
+A = 1
+KLD = 15.5 # For civil jets
+
+# Out
+L_Dmax = 1
+
+## Specific fuel consumption
+
+# Out
+SFC = 1
 
 ## Empty weight fraction
 
 # In
 
 # Out
-W_e_by_W_0 = 1
+We_W0 = 1
+
+## Initial fuel fraction
+
+# Out
+Winit_W0 = 1
+
+## Climb fuel fraction
+
+# Out
+Wclimb_Winit = 1
+
+## Cruise fuel fraction
+
+# Out
+Wcruise_Wclimb = 1
+
+## Descent fuel fraction
+
+# Out
+Wdescent_Wcruise = 1
+
+## Loiter fuel fraction
+
+# Out
+Wloiter_Wdescent = 1
+
+## Final fuel fraction
+
+# Out
+Wfinal_Wloiter = 1
+
+## Contingency fuel fractoin
+
+# Out
+Wcont_Wfinal = 1
+
+## Trapped fuel fraction
+
+# Out
+Wtrapped_Wcont = 1
+
+## Diversion fuel fraction
+
+# Out
+Wdiv_Wtrapped = 1
 
 ## Fuel weight fraction
 
