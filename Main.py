de759d97c069c98d16e659fb277bf13ab42e7d2a
IsakLundgren <isak.leopold@gmail.com>
2023-08-29 16:41:28 +0200

    Added some more blocks

--- Main.py
+++ Main.py
@@ -3,9 +3,18 @@ import matplotlib.pyplot as plt
 
 ### Predefined parameters
 
-W_passengers = 1 # kg
+passengerWeight = 105 # kg
+passengerCount = 90
 
 ### Calculations
+
+## Crew and payload weight calculation
+
+# In
+
+# Out
+W_crew = 1
+W_payload = 1
 
 ## Empty weight fraction
 
@@ -20,3 +29,14 @@ W_e_by_W_0 = 1
 
 # Out
 W_f_by_W_0 = 1
+
+## Take off weight
+
+# In
+W_crew = 1
+W_payload = 1
+W_e_by_W_0 = 1
+W_f_by_W_0 = 1
+
+# Out
+W_0 = (W_crew + W_payload) / (1 - W_e_by_W_0 - W_f_by_W_0)
