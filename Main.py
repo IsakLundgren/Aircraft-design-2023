3e732a6b7b894f54ea3ac8d2bda21044a9cd393c
IsakLundgren <isak.leopold@gmail.com>
2023-08-31 14:46:46 +0200

    Allotted work

--- Main.py
+++ Main.py
@@ -12,7 +12,7 @@ passengerCount = 90
 
 ### Calculations
 
-## Crew and payload weight calculation
+## Crew and payload weight calculation Isak
 
 # In
 crewCount = 5
@@ -21,81 +21,81 @@ crewCount = 5
 WCrew = 1
 WPayload = 1
 
-## Lift to drag ratio
+## Lift to drag ratio Isak
 
 # In
 Swet_Sref = 5.95
 A = 1
-KLD = 15.5 # For civil jets
+KLD = 15.5 # For civil jets Isak
 
 # Out
 L_Dmax = 1
 
-## Specific fuel consumption
+## Specific fuel consumption Isak
 
 # Out
 SFC = 1
 
-## Empty weight fraction
+## Empty weight fraction Isak
 
 # In
 
 # Out
 We_W0 = 1
 
-## Initial fuel fraction
+## Initial fuel fraction Isak
 
 # Out
 Winit_W0 = 1
 
-## Climb fuel fraction
+## Climb fuel fraction Mustafa
 
 # Out
 Wclimb_Winit = 1
 
-## Cruise fuel fraction
+## Cruise fuel fraction Mustafa
 
 # Out
 Wcruise_Wclimb = 1
 
-## Descent fuel fraction
+## Descent fuel fraction Mustafa
 
 # Out
 Wdescent_Wcruise = 1
 
-## Loiter fuel fraction
+## Loiter fuel fraction Mustafa
 
 # Out
 Wloiter_Wdescent = 1
 
-## Final fuel fraction
+## Final fuel fraction Mustafa
 
 # Out
 Wfinal_Wloiter = 1
 
-## Contingency fuel fractoin
+## Contingency fuel fraction Jay
 
 # Out
 Wcont_Wfinal = 1
 
-## Trapped fuel fraction
+## Trapped fuel fraction Jay
 
 # Out
 Wtrapped_Wcont = 1
 
-## Diversion fuel fraction
+## Diversion fuel fraction Jay
 
 # Out
 Wdiv_Wtrapped = 1
 
-## Fuel weight fraction
+## Fuel weight fraction Jay
 
 # In
 
 # Out
 W_f_by_W_0 = 1
 
-## Take off weight
+## Take off weight Jay
 
 # In
 W_crew = 1
