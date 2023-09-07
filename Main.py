ed72030c4608361777c35e06961403f3dda80c12
IsakLundgren <isak.leopold@gmail.com>
2023-09-07 15:54:42 +0200

    Changed naming issues in Jays part

--- Main.py
+++ Main.py
@@ -3,6 +3,14 @@ import pandas as pd
 from scipy.optimize import fsolve
 from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
+import warnings
+
+def fxn():
+    warnings.warn("deprecated", DeprecationWarning)
+
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore")
+    fxn()
 
 # Important notes:
 # Camel case for all parameters, underscore means fraction
@@ -31,7 +39,7 @@ Wpayload = passengerCount * passengerWeight
 # In
 Swet_Sref = 5.95
 print(f'Swet/Sref: {Swet_Sref:.3g}.')
-A = 9.16  # Aspect ratio http://www.b737.org.uk/techspecsdetailed.htm
+A = 12 # 9.16  # Aspect ratio http://www.b737.org.uk/techspecsdetailed.htm
 print(f'Aspect ratio: {A:.3g}.')
 KLD = 12  # For "Turboprop"
 
@@ -76,9 +84,9 @@ SFCH_SFCJetA = (SEH ** 3 * EDH * rhoH) / (SEJetA ** 3 * EDJetA * rhoJetA)
 ax.text(1941, 0.52e-7 * 10 ** 6, f'SFC_H by SFC_Jet A = {SFCH_SFCJetA:.3g}')
 
 # Finalize SFC calculation
-SFC = (param[0] / releaseYear + param[1]) * SFCH_SFCJetA
-print(f'SFC: {SFC * 10**6:.3g} mg/(Ws).')
-ax.scatter([releaseYear], [SFC * 10 ** 6],
+SFC_power = (param[0] / releaseYear + param[1]) * SFCH_SFCJetA
+print(f'SFC_power: {SFC_power * 10**6:.3g} mg/(Ws).')
+ax.scatter([releaseYear], [SFC_power * 10 ** 6],
            s=plt.rcParams['lines.markersize'] ** 2 * 2,
            c='k', marker='x', label='Model SFC (adjusted)')
 
@@ -118,15 +126,16 @@ rangeAircraft = 1100 * 1852  # metres   # convert 1100 nautical miles to metres
 cruise_mach = 0.5
 sound_speed = 309.696  # m/s  at FL250
 cruise_speed = cruise_mach * sound_speed
-SFC_cruise_power = 0.063/1000000  # kg/Ws Typical value of 0.07 for turboprop from historical data with 10% improvement
 efficiency_turboprop = 0.8  # From Raymer
-SFC_cruise = (SFC_cruise_power * cruise_speed) / efficiency_turboprop  # kg/Ns # Converted to turbojet equivalent
+SFC_cruise = (SFC_power * cruise_speed) / efficiency_turboprop  # kg/Ns # Converted to turbojet equivalent
+print(f'SFC: {SFC_cruise * 10**6:.3g} mg/Ns')
 L_Dcruise = L_Dmax
 
 Wcruise_Wclimb = np.exp((-rangeAircraft*SFC_cruise*gravity)/(cruise_speed * L_Dcruise))
 
 
 ## Loiter fuel fraction Mustafa
+# Included in cruise for initial sizing, set to 1
 Wdescent_Wcruise = 1
 
 
@@ -157,37 +166,27 @@ Wtrapped_Wcont = 1 """
 ## Diversion fuel fraction - Climb Jay
 
 # Out
-Wdiv_climb = 0.985
+WdivClimb_Wfinal = 0.985
 
 ## Diversion fuel fraction - Cruise Jay
 # In
-R = 1  # TODO Figure out what this is
-SFC_cruise = SFC  # TODO Might need some sort of ratio to this
-cruise_speed = 1  # TODO Set actual cruise speed
-L_Dcruise = L_Dmax  # TODO Might need some sort of ratio to this
+L_Dcruise = L_Dmax 
+rangeDiversion = 100 * 1852  #m
 
 # Out
-Wdiv_cruise = np.exp(((-R) * SFC_cruise) * gravity / (cruise_speed * L_Dcruise))
+WdivCruise_WdivClimb = np.exp(((-rangeDiversion) * SFC_cruise) * gravity / (cruise_speed * L_Dcruise))
 
 ## Diversion fuel fraction - Descent Jay
 
 # Out
-Wdiv_descent = 1  # TODO Find expression for this
-
-## Diversion fuel fraction TOTAL - Descent Jay
-# In
-# Wdiv_climb = 1
-# Wdiv_cruise = 1
-# Wdiv_descent = 1
-
-# Out
-Wdiv_final = Wdiv_climb * Wdiv_cruise * Wdiv_descent
+# Included in cruise for initial sizing, set to 1
+WdivDescent_WdivCruise = 1
 
 ## Fuel weight fraction Jay
 
 # In
 Wfinal_W0 = Wclimb_Winit * Wcruise_Wclimb * Wdescent_Wcruise * Wloiter_Wdescent * \
-            Wfinal_Wloiter * Wdiv_final
+            Wfinal_Wloiter * WdivClimb_Wfinal * WdivCruise_WdivClimb * WdivDescent_WdivCruise
 
 # Out
 Wf_W0 = 1.06 * (1-Wfinal_W0)
