121fe87569c9431e2efdd62959126d5c39a0fa14
IsakLundgren <isak.leopold@gmail.com>
2023-09-14 11:12:38 +0200

    Removed unnecessary comments

--- Main.py
+++ Main.py
@@ -21,24 +21,17 @@ gravity = 9.82
 ### Calculations
 
 ## Crew and payload weight calculation Isak
-
-# In
 crewCount = 5
-
-# Out
 Wcrew = crewCount * passengerWeight
 Wpayload = passengerCount * passengerWeight
 
 ## Lift to drag ratio Isak
-
-# In
 Swet_Sref = 6.1  # Provided as a suggestion for ATR-72, "Aircraft Design Studies Based on the ATR 72", https://www.fzt.haw-hamburg.de/pers/Scholz/arbeiten/TextNita.pdf
 print(f'Swet/Sref: {Swet_Sref:.3g}.')
 A = 12  # Aspect ratio https://www.rocketroute.com/aircraft/atr-72-212, https://en.wikipedia.org/wiki/ATR_72
 print(f'Aspect ratio: {A:.3g}.')
 KLD = 12  # For "Turboprop", Raymer 2018
 
-# Out
 L_Dmax = KLD * np.sqrt(A / Swet_Sref)
 print(f'L/D max: {L_Dmax:.3g}.')
 print(f'L/D cruise: {L_Dmax:.3g}.')
@@ -72,7 +65,8 @@ ax.plot(interpYear, interpSFC * 10 ** 6, '--r', label='Interpolation line')
 SEJetA = 43.15 * 10 ** 6  # J kg-1
 EDJetA = 34.7 * 10 ** 6 * 10 ** 3  # J m-3
 rhoJetA = 0.804 * 10 ** 3  # kg m-3
-LHVJetA = 43.39  # MJ kg-1 https://iashulf.memberclicks.net/technical-q-a-fuels
+# https://iashulf.memberclicks.net/technical-q-a-fuels
+LHVJetA = 0.0828 * 43.37 + 0.1328 * 43.38 + 0.7525 * 43.39 + 0.0318 * 43.246  # MJ kg-1
 SEH = 142 * 10 ** 6  # J kg-1 https://www.alakai.com/hydrogen-details
 EDH = 8 * 10 ** 6 * 10 ** 3  # J m-3 https://www.energy.gov/eere/fuelcells/hydrogen-storage
 rhoH = 0.071 * 10 ** 3 # kg m-3
@@ -101,24 +95,10 @@ fig.savefig('img/SFCYearRelation.png', dpi=figureDPI)
 
 ## Initial fuel fraction Isak
 
-# Out
 Winit_W0 = 0.97  # In lecture 1
-
-## Climb fuel fraction Mustafa
-
-# Out
-# Wclimb_Winit = 1
-
-## Cruise fuel fraction Mustafa
-
-# Out
-# Wcruise_Wclimb = 1
-
 Wclimb_Winit = 0.985  # From historical data
 
 ## Cruise fuel fraction Mustafa. The cruise fraction includes the descent part as mentioned in Raymer for initial sizing
-
-# Out
 rangeAircraft = 1100 * 1852  # metres   # convert 1100 nautical miles to metres
 cruise_mach = 0.5
 sound_speed = 309.696  # m/s  at FL250
@@ -135,8 +115,6 @@ Wcruise_Wclimb = np.exp((-rangeAircraft*SFC_cruise*gravity)/(cruise_speed * L_Dc
 # Included in cruise for initial sizing, set to 1
 Wdescent_Wcruise = 1
 
-
-# Out
 endurance = 20 * 60  # s #Converted from minutes
 loiter_speed = 200 * 0.514  # m/s # Converted from knots
 SFC_loiter_power = 0.101/1000000  # kg/Ws Typical value for turboprop from historical data Raymer. 0.08 given in slides
@@ -147,43 +125,31 @@ print(f'L/D loiter: {L_Dloiter:.3g}.')
 Wloiter_Wdescent = np.exp((-endurance*SFC_loiter*gravity)/L_Dcruise)
 
 ## Final fuel fraction Mustafa
-
-# Out
 Wfinal_Wloiter = 0.995  # from historical data
 
 ## Diversion fuel fraction - Climb Jay
-
-# Out
 WdivClimb_Wfinal = 0.985
 
 ## Diversion fuel fraction - Cruise Jay
 # Assumption: diversion takes place in FL250, M = 0.5, same as cruise conditions
-# In
 L_Dcruise = L_Dmax 
 rangeDiversion = 100 * 1852  #m
 
-# Out
 WdivCruise_WdivClimb = np.exp(((-rangeDiversion) * SFC_cruise) * gravity / (cruise_speed * L_Dcruise))
 
 ## Diversion fuel fraction - Descent Jay
 
-# Out
 # Included in cruise for initial sizing, set to 1
 WdivDescent_WdivCruise = 1
 
 ## Fuel weight fraction Jay
-
-# In
 Wfinal_W0 = Wclimb_Winit * Wcruise_Wclimb * Wdescent_Wcruise * Wloiter_Wdescent * \
             Wfinal_Wloiter * WdivClimb_Wfinal * WdivCruise_WdivClimb * WdivDescent_WdivCruise
 
-# Out
 Wf_W0 = 1.06 * (1-Wfinal_W0)
 print(f'FUEL/MTOW: {Wf_W0:.3g}.')
 
 ## Empty weight fraction Isak
-
-# In
 futureTechNudgeFactor = 0.96  # Design guess
 hydrogenTanksNudgeFactor = 1.12  # Design guess
 a = 0.92 * futureTechNudgeFactor * hydrogenTanksNudgeFactor
@@ -195,13 +161,10 @@ def We_frac_equation(We_W0_inner):
     return a * ((Wcrew + Wpayload) / (1 - Wf_W0 - We_W0_inner)) ** c - We_W0_inner
 
 
-# Out
 We_W0 = float(fsolve(We_frac_equation, We_W0initialGuess))
 print(f'OEW/MTOW: {We_W0:.3g}.')
 
 ## Take off weight Jay
-
-# Out
 W0 = (Wcrew + Wpayload) / (1 - We_W0 - Wf_W0)
 print(f'MTOW: {W0 * 10 ** -3:.3g} tonnes.')
 
