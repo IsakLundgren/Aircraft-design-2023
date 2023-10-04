dfeb02fbda20137089a9b7c09fd97112c4a35e4a
isak lundgren <isak.leopold@gmail.com>
2023-10-04 14:49:57 +0200

    Using different way of approximating SFC

--- Main.py
+++ Main.py
@@ -56,34 +56,27 @@ SFCData = dfSFC.loc[:, 'SFC kg/Ws']
 
 ax.scatter(yearData, SFCData * 10 ** 6, c='b', label='Reference data')
 
-
 # Create curvefit
-def mockFunction(year, numerator, term):
-    return numerator / year + term
-
-
 # noinspection PyTupleAssignmentBalance
-param, param_cov = curve_fit(mockFunction, yearData, SFCData)
+param = np.polyfit(yearData, np.log(SFCData), 1)
 interpYear = np.linspace(1940, 2050, 1000)
-interpSFC = param[0] / interpYear + param[1]
+interpSFC = np.exp(param[1]) * np.exp(param[0] * interpYear)
 ax.plot(interpYear, interpSFC * 10 ** 6, '--r', label='Interpolation line')
 
 # Find sfcH_sfcjetA
 # Jet A info from https://en.wikipedia.org/wiki/Jet_fuel
-SEJetA = 43.15 * 10 ** 6  # J kg-1
-EDJetA = 34.7 * 10 ** 6 * 10 ** 3  # J m-3
 rhoJetA = 0.804 * 10 ** 3  # kg m-3
 # https://iashulf.memberclicks.net/technical-q-a-fuels
 LHVJetA = 0.0828 * 43.37 + 0.1328 * 43.38 + 0.7525 * 43.39 + 0.0318 * 43.246  # MJ kg-1
-SEH = 142 * 10 ** 6  # J kg-1 https://www.alakai.com/hydrogen-details
-EDH = 8 * 10 ** 6 * 10 ** 3  # J m-3 https://www.energy.gov/eere/fuelcells/hydrogen-storage
+
 rhoH = 0.071 * 10 ** 3  # kg m-3
 LHVH = 119.96  # MJ kg-1 https://h2tools.org/hyarc/calculator-tools/lower-and-higher-heating-values-fuels
 
 SFCH_SFCJetA = LHVJetA / LHVH
 
 # Finalize SFC calculation
-SFC_power = (param[0] / releaseYear + param[1]) * SFCH_SFCJetA
+SFCCruiseAdjustment = 1 - 0.1
+SFC_power = np.exp(param[1]) * np.exp(param[0] * releaseYear) * SFCH_SFCJetA * SFCCruiseAdjustment
 print(f'SFC_power: {SFC_power * 10**6:.3g} mg/(Ws).')
 ax.scatter([releaseYear], [SFC_power * 10 ** 6],
            s=plt.rcParams['lines.markersize'] ** 2 * 2,
